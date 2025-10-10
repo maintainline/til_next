7d8d3aad8ed81de6f16c17ceaf6052338ccb784b
maintainline <z.seon.dev@gmail.com>
2025-10-10 12:52:07 +0900

    [docs] Next.js 의 라우터 및 레이아웃 이해

---
+++ src/app/good/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+async function Page({ params }: PageProps) {
+  const { id } = await params;
+  return <div>{id} 번 상품</div>;
+}
+
+export default Page;
---
+++ src/app/good/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>상품</div>;
+}
+
+export default Page;
