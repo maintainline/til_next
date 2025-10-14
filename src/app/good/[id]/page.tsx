4b272f5459aef08d745b0b2adb3ef93bbf4d37cc
maintainline <z.seon.dev@gmail.com>
2025-10-14 12:14:53 +0900

    [docs] router cache 의 dlgo

--- src/app/good/[id]/page.tsx
+++ src/app/good/[id]/page.tsx
@@ -2,16 +2,10 @@ import styles from "@/app/good/[id]/page.module.css";
 import { GoodDataType } from "@/types/types";
 import Image from "next/image";
 
-const mockData: GoodDataType = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-  rating: { rate: 3.9, count: 120 },
-};
+// 약속된 Next 함수임(미리 페이지를 Static 페이지 이고, SSR 페이지 이다.)
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 interface PageProps {
   params: Promise<{ id: string }>;
@@ -24,7 +18,7 @@ async function Page({ params }: PageProps) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`
   );
-  
+
   const good: GoodDataType = await response.json();
   const { title, image, category, description, rating } = good;
 
