0047e51c491668b210f154c9f787b2b54522b73c
maintainline <z.seon.dev@gmail.com>
2025-10-15 11:06:48 +0900

    [docs] loading 처리의 이해

---
+++ src/components/skeleton/GoodItemListSkeleton.tsx
@@ -0,0 +1,13 @@
+import GoodItemSkeleton from "./GoodItemSkeleton";
+
+interface GoodItemListSkeletonProps {
+  count: number;
+}
+
+const GoodItemListSkeleton = ({ count }: GoodItemListSkeletonProps) => {
+  return new Array(count)
+    .fill(0)
+    .map((_, index) => <GoodItemSkeleton key={index} />);
+};
+
+export default GoodItemListSkeleton;
